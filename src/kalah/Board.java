cb77fd3c6c65b59bd9ae6191c34c1cb232643976
Xiaohui Lin <xiaohui@outlook.co.nz>
2017-05-08 11:52:54 +1200

    Move game logic into the Board class

---
+++ src/kalah/Board.java
@@ -0,0 +1,108 @@
+package kalah;
+
+import com.qualitascorpus.testsupport.IO;
+import com.sun.istack.internal.NotNull;
+import kalah.util.AsciiUtil;
+
+import java.io.IOException;
+
+/**
+ * A class that represents the Mancala board.
+ */
+public class Board {
+
+	@NotNull private Player mPlayer1 = new Player();
+	@NotNull private Player mPlayer2 = new Player();
+	@NotNull private Player mCurrentPlayer = mPlayer1;
+
+	// update board method
+	void update(int houseNumberSelected) throws IOException {
+
+		int numberOfSeeds = mCurrentPlayer.getHouse(houseNumberSelected - 1).getSeeds();
+		mCurrentPlayer.getHouse(houseNumberSelected - 1).setSeeds(0);
+
+		int lastBoardIndex = 0; // the index of the global board in which the last seed was sown
+
+		if (numberOfSeeds == 0) {
+			throw new IOException("House has no seeds");
+		} else {
+			Player playerBoardToDistributeOn = mCurrentPlayer;
+
+			for (int currentIndex = houseNumberSelected; numberOfSeeds > 0; currentIndex++) {
+				lastBoardIndex = currentIndex;
+
+				if (currentIndex <= Player.NUMBER_OF_HOUSES - 1) {
+					playerBoardToDistributeOn.getHouse(currentIndex).addSeeds(1);
+					numberOfSeeds--;
+					if (numberOfSeeds == 0) break;
+				}
+
+				// check if we have reached the end of the board
+				if (currentIndex >= Player.NUMBER_OF_HOUSES - 1) {
+					// check if we can place it in the store
+					boolean isStoreCurrentPlayers = playerBoardToDistributeOn.equals(mCurrentPlayer);
+					if (isStoreCurrentPlayers) {
+						playerBoardToDistributeOn.getStore().addSeeds(1);
+						numberOfSeeds--;
+						lastBoardIndex = -1;
+					}
+					// swap player's board if the current index is the last index on the board
+					playerBoardToDistributeOn = playerBoardToDistributeOn.equals(mPlayer1) ? mPlayer2 : mPlayer1;
+					currentIndex = -1; // the current index should be 0, but set it to -1 as the for
+					// loop is going to increment it again
+				}
+			}
+
+			// check if the current player gets another turn
+			// if the last seed sown is in the players store
+			if (lastBoardIndex != -1) {
+				// check for capture, i.e. when you land on your own board that had no seeds in it
+				if (playerBoardToDistributeOn.equals(mCurrentPlayer) &&
+						playerBoardToDistributeOn.getHouse(lastBoardIndex).getSeeds() == 1) {
+					Player playerToCaptureFrom = mCurrentPlayer.equals(mPlayer1) ? mPlayer2 : mPlayer1;
+					House oppositeHouse = playerToCaptureFrom.getHouse(Player.NUMBER_OF_HOUSES - 1 -
+							lastBoardIndex);
+					if (oppositeHouse.getSeeds() != 0) {
+						mCurrentPlayer.getStore().addSeeds(oppositeHouse.getSeeds() + 1);
+						oppositeHouse.setSeeds(0);
+						mCurrentPlayer.getHouse(lastBoardIndex).setSeeds(0);
+					}
+				}
+				mCurrentPlayer = mCurrentPlayer.equals(mPlayer1) ? mPlayer2 : mPlayer1;
+			}
+		}
+	}
+
+	Player getPlayer1() {
+		return mPlayer1;
+	}
+
+	Player getPlayer2() {
+		return mPlayer2;
+	}
+
+	Player getCurrentPlayer() {
+		return mCurrentPlayer;
+	}
+
+	/**
+	 * Determine if the game has finished
+	 *
+	 * @return True if the current player has no more seeds to play, false if otherwise
+	 */
+	boolean isGameFinished() {
+		boolean isFinished = true;
+		for (House house : mCurrentPlayer.getHouses()) {
+			if (house.getSeeds() != 0) isFinished = false;
+		}
+		return isFinished;
+	}
+
+	void print(IO io) {
+		io.println(AsciiUtil.getLineDecor1());
+		io.println(AsciiUtil.getTopBoard(mPlayer2.getHouses(), mPlayer1.getStore().getSeeds()));
+		io.println(AsciiUtil.getLineDecor2());
+		io.println(AsciiUtil.getBottomBoard(mPlayer1.getHouses(), mPlayer2.getStore().getSeeds()));
+		io.println(AsciiUtil.getLineDecor1());
+	}
+}
--- src/kalah/Kalah.java
+++ src/kalah/Kalah.java
@@ -2,12 +2,9 @@ package kalah;
 
 import com.qualitascorpus.testsupport.IO;
 import com.qualitascorpus.testsupport.MockIO;
-import com.sun.istack.internal.NotNull;
-import kalah.model.Pit;
 import kalah.util.AsciiUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 /**
  * This class is the starting point for the Modifiability Assignment.
@@ -18,10 +15,6 @@ public class Kalah {
 	private static int MIN_HOUSE_NUMBER = 1;
 	private static int MAX_HOUSE_NUMBER = Player.NUMBER_OF_HOUSES;
 
-	@NotNull Player player1 = new Player();
-	@NotNull Player player2 = new Player();
-	@NotNull private Player mCurrentPlayer = player1;
-
 	public static void main(String[] args) {
 		new Kalah().play(new MockIO());
 	}
@@ -29,76 +22,35 @@ public class Kalah {
 	public void play(IO io) {
 
 		int houseNumberSelected = 0;
+		Board board = new Board();
 
 		while (true) {
-			printBoard(io);
 
-			if (isGameFinished()) break;
+			board.print(io);
 
-			houseNumberSelected = io.readInteger(mCurrentPlayer.equals(player1) ? AsciiUtil.getPlayer1Turn() :
-					AsciiUtil.getPlayer2Turn(), MIN_HOUSE_NUMBER, MAX_HOUSE_NUMBER, CANCEL_CODE, "q");
+			if (board.isGameFinished()) break;
+
+			Player mCurrentPlayer = board.getCurrentPlayer();
+			houseNumberSelected = io.readInteger(mCurrentPlayer.equals(board.getPlayer1()) ? AsciiUtil.getPlayer1Turn
+					() : AsciiUtil.getPlayer2Turn(), MIN_HOUSE_NUMBER, MAX_HOUSE_NUMBER, CANCEL_CODE, "q");
 
 			if (houseNumberSelected == CANCEL_CODE) break;
 
-			int numberOfSeeds = mCurrentPlayer.getHouse(houseNumberSelected - 1).getSeeds();
-			mCurrentPlayer.getHouse(houseNumberSelected - 1).setSeeds(0);
-
-			int lastBoardIndex = 0; // the index of the global board in which the last seed was sown
-
-			if (numberOfSeeds == 0) {
+			try {
+				board.update(houseNumberSelected);
+			} catch (IOException e) {
+				e.getMessage();
 				io.println("House is empty. Move again.");
-			} else {
-				Player playerBoardToDistributeOn = mCurrentPlayer;
-
-				for (int currentIndex = houseNumberSelected; numberOfSeeds > 0; currentIndex++) {
-					lastBoardIndex = currentIndex;
-
-					if (currentIndex <= Player.NUMBER_OF_HOUSES - 1) {
-						playerBoardToDistributeOn.getHouse(currentIndex).addSeeds(1);
-						numberOfSeeds--;
-						if (numberOfSeeds == 0) break;
-					}
-
-					// check if we have reached the end of the board
-					if (currentIndex >= Player.NUMBER_OF_HOUSES - 1) {
-						// check if we can place it in the store
-						boolean isStoreCurrentPlayers = playerBoardToDistributeOn.equals(mCurrentPlayer);
-						if (isStoreCurrentPlayers) {
-							playerBoardToDistributeOn.getStore().addSeeds(1);
-							numberOfSeeds--;
-							lastBoardIndex = -1;
-						}
-						// swap player's board if the current index is the last index on the board
-						playerBoardToDistributeOn = playerBoardToDistributeOn.equals(player1) ? player2 : player1;
-						currentIndex = -1; // the current index should be 0, but set it to -1 as the for
-						// loop is going to increment it again
-					}
-				}
-
-				// check if the current playerBoardToDistributeOn gets another turn
-				// if the last seed sown is in the players store
-				if (lastBoardIndex != -1) {
-					// check for capture, i.e. when you land on your own board that had no seeds in it
-					if (playerBoardToDistributeOn.equals(mCurrentPlayer) &&
-							playerBoardToDistributeOn.getHouse(lastBoardIndex).getSeeds() == 1) {
-						Player playerToCaptureFrom = mCurrentPlayer.equals(player1) ? player2 : player1;
-						House oppositeHouse = playerToCaptureFrom.getHouse(Player.NUMBER_OF_HOUSES - 1 -
-								lastBoardIndex);
-						if (oppositeHouse.getSeeds() != 0) {
-							mCurrentPlayer.getStore().addSeeds(oppositeHouse.getSeeds() + 1);
-							oppositeHouse.setSeeds(0);
-							mCurrentPlayer.getHouse(lastBoardIndex).setSeeds(0);
-						}
-					}
-					mCurrentPlayer = mCurrentPlayer.equals(player1) ? player2 : player1;
-				}
 			}
 		}
 
 		io.println("Game over");
-		printBoard(io);
+		board.print(io);
 
 		if (houseNumberSelected != CANCEL_CODE) {
+			Player player1 = board.getPlayer1();
+			Player player2 = board.getPlayer2();
+
 			io.println("\tplayer 1:" + player1.getScore());
 			io.println("\tplayer 2:" + player2.getScore());
 
@@ -110,26 +62,5 @@ public class Kalah {
 				io.println("A tie!");
 			}
 		}
-	}
-
-	/**
-	 * Determine if the game has finished
-	 *
-	 * @return True if the current player has no more seeds to play, false if otherwise
-	 */
-	private boolean isGameFinished() {
-		boolean isFinished = true;
-		for (House house : mCurrentPlayer.getHouses()) {
-			if (house.getSeeds() != 0) isFinished = false;
-		}
-		return isFinished;
-	}
-
-	private void printBoard(IO io) {
-		io.println(AsciiUtil.getLineDecor1());
-		io.println(AsciiUtil.getTopBoard(player2.getHouses(), player1.getStore().getSeeds()));
-		io.println(AsciiUtil.getLineDecor2());
-		io.println(AsciiUtil.getBottomBoard(player1.getHouses(), player2.getStore().getSeeds()));
-		io.println(AsciiUtil.getLineDecor1());
 	}
 }
