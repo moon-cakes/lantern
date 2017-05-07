f240e4f7502de50ea3d2d25c2eab8c028a743f41
Xiaohui Lin <xiaohui@outlook.co.nz>
2017-05-07 12:10:15 +1200

    Add getHouse methods

--- src/kalah/Player.java
+++ src/kalah/Player.java
@@ -4,34 +4,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A player who partakes in the game
+ * A player who plays the game and had a score. A player has 6 {@link House's} and a {@link Store}.
  */
 public class Player {
 
-    private static int NUMBER_OF_HOUSES = 6;
+	static final int NUMBER_OF_HOUSES = 6;
 
-    private List<House> mHouses = new ArrayList<>();
-    private Store mStore;
-    private int mScore;
+	private List<House> mHouses = new ArrayList<>();
+	private Store mStore = new Store();
+	private int mScore;
 
-    public Player() {
-        for (int i = 0; i < NUMBER_OF_HOUSES; i++) {
-            mHouses.add(new House());
-        }
-        this.mHouses = mHouses;
-        this.mStore = mStore;
-    }
+	public Player() {
+		for (int i = 0; i < NUMBER_OF_HOUSES; i++) {
+			mHouses.add(new House());
+		}
+		mStore.setOwner(this);
+	}
 
-    /**
-     * Add the number of seeds collected to a players score
-     *
-     * @param seedsCollected The number of seeds collected in one round
-     */
-    public void addSeeds(int seedsCollected) {
-        mScore += seedsCollected;
-    }
+	/**
+	 * Returns the player's store
+	 *
+	 * @return
+	 */
+	public Store getStore() {
+		return mStore;
+	}
 
-    public int getScore() {
-        return mScore;
-    }
+	/**
+	 * Return a House for the houseNumber
+	 *
+	 * @param houseNumber The houseNumber of the house to be retrieved
+	 * @return a House according to a house number                                                         /
+	 */
+	public House getHouse(int houseNumber) {
+		return mHouses.get(houseNumber);
+	}
+
+	/**
+	 * Add the number of seeds collected to a players score
+	 *
+	 * @param seedsCollected The number of seeds collected in one round
+	 */
+	public void addToScore(int seedsCollected) {
+		mScore += seedsCollected;
+	}
+
+	public int getScore() {
+		return mScore;
+	}
+
 }
